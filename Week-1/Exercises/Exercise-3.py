27dba41ec2db5115d542d51227bdb4b946d16c21
Imane ROUIDI <imanrouidi.ir@gmail.com>
2025-09-21 01:54:38 +0200

    Added answers to Exercise 2

--- Week-1/Exercises/Exercise-3.py
+++ Week-1/Exercises/Exercise-3.py
@@ -19,7 +19,7 @@ lst = [1, 4, -6, 7, 2, 3, 9, 11, 6] # In all exercises in this script, you will 
 
 print("Exercise 3.1")
 
-pass
+print(sum(lst))
 
 print("---")
 
@@ -33,7 +33,10 @@ Print the product of the elements in the list.
 
 print("Exercise 3.2")
 
-pass
+product = 1
+for x in lst:
+    product *= x
+print(product)
 
 print("---")
 
@@ -47,7 +50,10 @@ Print the sum of the squares of the list.
 
 print("Exercise 3.3")
 
-pass
+squares_sum = 0
+for x in lst:
+    squares_sum += x**2
+print(squares_sum)
 
 print("---")
 
@@ -61,7 +67,7 @@ Print the largest element of the list.
 
 print("Exercise 3.4")
 
-pass
+print(max(lst))
 
 print("---")
 
@@ -75,6 +81,6 @@ Print the largest element of the list.
 
 print("Exercise 3.5")
 
-pass
+print(max(lst))
 
 print("---")
