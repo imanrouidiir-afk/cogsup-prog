5466032025fe3b73dd8bb83347c00f2c40bf8bfe
imanrouidiir-afk <imanrouidi.ir@gmail.com>
2025-10-09 13:56:42 +0200

    Update blindspot.py

--- Week-5/Exercises/blindspot.py
+++ Week-5/Exercises/blindspot.py
@@ -15,29 +15,62 @@ def make_circle(r, pos=(0,0)):
 """ Experiment """
 def run_trial(side="left"):
     instruction_text = (
-        f"Cover your {side} eye.\n\n"
-        "Fixate on the black cross.\n\n"
-        "Use the ARROW keys to move the circle.\n"
-        "Press 1 to make it smaller, 2 to make it larger.\n\n"
+        f"Cover your {side} eye."
+        " "
+        "Fixate on the black cross."
+        "Use the ARROW keys to move the circle."
+        "Press 1 to make it smaller, 2 to make it larger."
         "When the circle disappears (blind spot reached), press SPACE."
     )
     instructions = stimuli.TextScreen("Instructions", instruction_text)
     instructions.present()
     exp.keyboard.wait_char(' ')
     
-    fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=[300, 0])
+    fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=[300, 0] if side == "left" else [-300, 0])
     fixation.preload()
 
     radius = 75
+    circle_pos = [0, 0]
     circle = make_circle(radius)
 
     fixation.present(True, False)
     circle.present(False, True)
+     running = True
+    while running:
+        fixation.present(clear=True)
+        circle.present(clear=False)
+        exp.screen.update()
+        exp.clock.wait(10)
 
-    exp.keyboard.wait()
+     key = exp.keyboard.wait()
+            if key:
+            
+            if key == K_LEFT:
+                circle_pos[0] -= 10
+            if key == K_RIGHT:
+                circle_pos[0] += 10
+            if key == K_UP:
+                circle_pos[1] += 10
+            if key == K_DOWN:
+                circle_pos[1] -= 10
+
+            if key == ord('1'):
+                radius -= 5
+            if key == ord('2'):
+                radius += 5
+            
+            if key == ord(' '):
+                running = False
+
+            
+            circle = make_circle(radius, circle_pos)
 
 control.start(subject_id=1)
 
-run_trial()
-    
+run_trial("L")
+run_trial("R") 
+
+stimuli.TextScreen("Done!", "You finished the Test.\n\nThank u:)").present()
+exp.keyboard.wait()
+
 control.end()
